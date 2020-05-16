4a3283dd31b6becbab39a6f4e50eb81097751b48
bopat <bopat@DESKTOP-QHIOA3R.fritz.box>
2020-05-16 16:23:43 +0200

    initial commit

---
+++ RadarDataSaver/src/main.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import socket
+import collections
+
+# def writeToFile(txt):
+#     f = open(filename, "a")
+#     f.write(txt)
+#     f.close()    
+
+filename = datetime.now()
+filename = filename.replace(microsecond=0)
+filename = str(filename)
+filename = filename.replace(":", "-")
+filename = filename.replace(" ", "_")
+filename = filename+".txt"
+f = open(filename, "w")
+f.close()
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 13377   
+
+print("data dump active...: ")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while(True):
+        #data = s.recv(int(2**10))
+        deque = collections.deque([], 6)
+        data = ""
+        while True:
+            char = s.recv(1).decode()            
+            data += char
+            deque.append(char)
+            terminator = str().join(deque)            
+            if terminator == "</DGM>":
+                break
+        timestamp = str(datetime.now())
+        with open(filename, "a") as f:
+            f.write(timestamp+"\n")
+            f.write(data)            
+        
+#         writeToFile(timestamp+"\n")
+#         writeToFile(data.decode())
